8901fd4d26246a7938e3c376eddbd328cba1def8
undertaker28 <pavel.starovoytovvv@gmail.com>
2023-08-11 14:18:17 +0300

    Initial Commit

---
+++ QRCodeScanner/QRCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeScannerApp.swift
+//  QRCodeScanner
+//
+//  Created by Pavel on 11.08.23.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
